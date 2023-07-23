e916eee11affa61ada1106a39e6f4049d2406929
Fredi Kats <fredikats@outlook.com>
2023-07-23 15:15:58 +0400

    Add Directory.Build.props for forcing same lang level for solution

--- Sources/Kysect.Configuin.Common/ServiceCollectionExtensions.cs
+++ Sources/Kysect.Configuin.Common/ServiceCollectionExtensions.cs
@@ -1,21 +1,20 @@
 ﻿using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Options;
 
-namespace Kysect.Configuin.Common
+namespace Kysect.Configuin.Common;
+
+public static class ServiceCollectionExtensions
 {
-    public static class ServiceCollectionExtensions
+    public static IServiceCollection AddOptionsWithValidation<T>(this IServiceCollection serviceCollection, string name, bool validateOnStart = true) where T : class
     {
-        public static IServiceCollection AddOptionsWithValidation<T>(this IServiceCollection serviceCollection, string name, bool validateOnStart = true) where T : class
-        {
-            OptionsBuilder<T> builder = serviceCollection
-                .AddOptions<T>()
-                .BindConfiguration(name)
-                .ValidateDataAnnotations();
+        OptionsBuilder<T> builder = serviceCollection
+            .AddOptions<T>()
+            .BindConfiguration(name)
+            .ValidateDataAnnotations();
 
-            if (validateOnStart)
-                builder = builder.ValidateOnStart();
+        if (validateOnStart)
+            builder = builder.ValidateOnStart();
 
-            return builder.Services;
-        }
+        return builder.Services;
     }
 }
