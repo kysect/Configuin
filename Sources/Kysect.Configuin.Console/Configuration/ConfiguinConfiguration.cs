9cb4509b423bed9906a7445cffdd3c008e6f1ede
Fredi Kats <fredikats@outlook.com>
2023-07-23 23:23:33 +0400

    Apply code review suggestions

--- Sources/Kysect.Configuin.Console/Configuration/ConfiguinConfiguration.cs
+++ Sources/Kysect.Configuin.Console/Configuration/ConfiguinConfiguration.cs
@@ -1,11 +1,9 @@
 ﻿using System.ComponentModel.DataAnnotations;
-using System.Diagnostics.CodeAnalysis;
 
 namespace Kysect.Configuin.Console.Configuration;
 
-[SuppressMessage("Naming", "CS8618:Non-nullable variable must contain a non-null value when exiting constructor", Justification = "AutoValidation")]
 public class ConfiguinConfiguration
 {
     [Required]
-    public string MsLearnRepositoryPath { get; init; }
+    public string MsLearnRepositoryPath { get; init; } = null!;
 }
