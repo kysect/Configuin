921c9930652f4f00771cdc72e87148e183640eb1
Fredi Kats <fredikats@outlook.com>
2023-07-23 14:48:56 +0400

    Add configuration via appsettings.json

---
+++ Sources/Kysect.Configuin.Console/Configuration/ConfiguinConfiguration.cs
@@ -0,0 +1,11 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.Diagnostics.CodeAnalysis;
+
+namespace Kysect.Configuin.Console.Configuration;
+
+[SuppressMessage("Naming", "CS8618:Non-nullable variable must contain a non-null value when exiting constructor", Justification = "AutoValidation")]
+public class ConfiguinConfiguration
+{
+    [Required]
+    public string MsLearnRepositoryPath { get; init; }
+}
