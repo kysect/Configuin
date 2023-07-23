ac5c30bcd3ec3d7c40baa8657da7f4a2a422d99a
Fredi Kats <fredikats@outlook.com>
2023-07-23 14:59:21 +0400

    Add implementation for reading rule descriptions from ms learn

--- Sources/Kysect.Configuin.Core/MsLearnDocumentation/Models/MsLearnDocumentationRawInfo.cs
+++ Sources/Kysect.Configuin.Core/MsLearnDocumentation/Models/MsLearnDocumentationRawInfo.cs
@@ -4,10 +4,23 @@ public class MsLearnDocumentationRawInfo
 {
     public IReadOnlyCollection<string> QualityRuleInfos { get; }
     public IReadOnlyCollection<string> StyleRuleInfos { get; }
+    public string SharpFormattingOptionsContent { get; }
+    public string DotnetFormattingOptionsContent { get; }
 
-    public MsLearnDocumentationRawInfo(IReadOnlyCollection<string> qualityRuleInfos, IReadOnlyCollection<string> styleRuleInfos)
+    public MsLearnDocumentationRawInfo(
+        IReadOnlyCollection<string> qualityRuleInfos,
+        IReadOnlyCollection<string> styleRuleInfos,
+        string sharpFormattingOptionsContent,
+        string dotnetFormattingOptionsContent)
     {
+        ArgumentNullException.ThrowIfNull(qualityRuleInfos);
+        ArgumentNullException.ThrowIfNull(styleRuleInfos);
+        ArgumentException.ThrowIfNullOrEmpty(sharpFormattingOptionsContent);
+        ArgumentException.ThrowIfNullOrEmpty(dotnetFormattingOptionsContent);
+
         QualityRuleInfos = qualityRuleInfos;
         StyleRuleInfos = styleRuleInfos;
+        SharpFormattingOptionsContent = sharpFormattingOptionsContent;
+        DotnetFormattingOptionsContent = dotnetFormattingOptionsContent;
     }
 }
