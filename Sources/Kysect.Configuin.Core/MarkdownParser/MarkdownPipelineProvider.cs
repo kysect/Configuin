859cc9ca0239958f1c9f910d68633056ffb051bf
Fredi Kats <fredikats@outlook.com>
2023-07-24 14:46:43 +0400

    Implement markdown table parsing from MS Learn

---
+++ Sources/Kysect.Configuin.Core/MarkdownParser/MarkdownPipelineProvider.cs
@@ -0,0 +1,11 @@
+﻿using Markdig;
+
+namespace Kysect.Configuin.Core.MarkdownParser;
+
+public class MarkdownPipelineProvider
+{
+    public static MarkdownPipeline GetDefault()
+    {
+        return new MarkdownPipelineBuilder().UseAdvancedExtensions().EnableTrackTrivia().Build();
+    }
+}
---
+++ Sources/Kysect.Configuin.Core/MarkdownParser/Tables/Models/MarkdownPropertyValueWithDescriptionTable.cs
@@ -0,0 +1,11 @@
+﻿namespace Kysect.Configuin.Core.MarkdownParser.Tables.Models;
+
+public class MarkdownPropertyValueWithDescriptionTable
+{
+    public Dictionary<string, IReadOnlyList<MarkdownPropertyValueWithDescriptionTableRow>> Rows { get; }
+
+    public MarkdownPropertyValueWithDescriptionTable(Dictionary<string, IReadOnlyList<MarkdownPropertyValueWithDescriptionTableRow>> rows)
+    {
+        Rows = rows;
+    }
+}
