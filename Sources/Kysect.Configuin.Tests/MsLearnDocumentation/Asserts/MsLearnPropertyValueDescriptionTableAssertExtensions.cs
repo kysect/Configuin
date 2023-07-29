92c1eea7a2e42048befb3ea4ad5f7bc82c80cceb
Fredi Kats <fredikats@outlook.com>
2023-07-29 11:38:19 +0400

    Remove assert wrappers for ms learn tables

---
+++ Sources/Kysect.Configuin.Tests/MsLearnDocumentation/Asserts/MsLearnPropertyValueDescriptionTableAssertExtensions.cs
@@ -0,0 +1,16 @@
+﻿using FluentAssertions;
+using FluentAssertions.Collections;
+using Kysect.Configuin.Core.MsLearnDocumentation.Tables.Models;
+
+namespace Kysect.Configuin.Tests.MsLearnDocumentation.Asserts;
+
+public static class MsLearnPropertyValueDescriptionTableAssertExtensions
+{
+    public static AndWhichConstraint<GenericCollectionAssertions<MsLearnPropertyValueDescriptionTableRow>, MsLearnPropertyValueDescriptionTableRow> Contain(
+        this GenericCollectionAssertions<MsLearnPropertyValueDescriptionTableRow> assert,
+        string value,
+        string? description = null)
+    {
+        return assert.Contain(new MsLearnPropertyValueDescriptionTableRow(value, description ?? string.Empty));
+    }
+}
