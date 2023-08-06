024be2e2f0300bab8bcaac79083e4d5f1542d7bd
Fredi Kats <fredikats@outlook.com>
2023-08-06 15:27:41 +0400

    Add test for validation ms learn submodule

---
+++ Sources/Kysect.Configuin.Tests/MsLearnRepositoryPathProviderTests.cs
@@ -0,0 +1,33 @@
+﻿using FluentAssertions;
+using Kysect.Configuin.Core.MsLearnDocumentation;
+using NUnit.Framework;
+
+namespace Kysect.Configuin.Tests;
+
+public class MsLearnRepositoryPathProviderTests
+{
+    [Test]
+    public void GetPath_ReturnExistsFileItems()
+    {
+        string pathToRoot = Path.Combine(
+            "..", // net7.0
+            "..", // Debug
+            "..", // bin
+            "..", // Kysect.Configuin.Tests
+            "..", // root
+            "ms-learn");
+
+        var pathProvider = new MsLearnRepositoryPathProvider(pathToRoot);
+
+        string pathToStyleRules = pathProvider.GetPathToStyleRules();
+        string pathToQualityRules = pathProvider.GetPathToQualityRules();
+        string pathToSharpFormattingFile = pathProvider.GetPathToSharpFormattingFile();
+        string pathToDotnetFormattingFile = pathProvider.GetPathToDotnetFormattingFile();
+
+        Directory.Exists(pathToStyleRules).Should().BeTrue($"Directory {pathToStyleRules} must exist");
+        Directory.Exists(pathToQualityRules).Should().BeTrue($"Directory {pathToQualityRules} must exist");
+
+        File.Exists(pathToSharpFormattingFile).Should().BeTrue($"File {pathToSharpFormattingFile} must exist");
+        File.Exists(pathToDotnetFormattingFile).Should().BeTrue($"File {pathToDotnetFormattingFile} must exist");
+    }
+}
