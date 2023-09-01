dafbf607ef59aa704329ceed3d571157b607e973
Fredi Kats <fredikats@outlook.com>
2023-09-01 23:52:03 +0400

    Add dotnet and csharp formatting options to RoslynRules

--- Sources/Kysect.Configuin.Core/RoslynRuleModels/RoslynRules.cs
+++ Sources/Kysect.Configuin.Core/RoslynRuleModels/RoslynRules.cs
@@ -4,11 +4,19 @@ public class RoslynRules
 {
     public IReadOnlyCollection<RoslynQualityRule> QualityRules { get; }
     public IReadOnlyCollection<RoslynStyleRule> StyleRules { get; }
+    public IReadOnlyCollection<RoslynStyleRuleOption> DotnetFormattingOptions { get; }
+    public IReadOnlyCollection<RoslynStyleRuleOption> SharpFormattingOptions { get; }
 
-    public RoslynRules(IReadOnlyCollection<RoslynQualityRule> qualityRules, IReadOnlyCollection<RoslynStyleRule> styleRules)
+    public RoslynRules(
+        IReadOnlyCollection<RoslynQualityRule> qualityRules,
+        IReadOnlyCollection<RoslynStyleRule> styleRules,
+        IReadOnlyCollection<RoslynStyleRuleOption> dotnetFormattingOptions,
+        IReadOnlyCollection<RoslynStyleRuleOption> sharpFormattingOptions)
     {
         QualityRules = qualityRules;
         StyleRules = styleRules;
+        DotnetFormattingOptions = dotnetFormattingOptions;
+        SharpFormattingOptions = sharpFormattingOptions;
     }
 
     public IReadOnlyCollection<RoslynStyleRuleOption> GetOptions()
