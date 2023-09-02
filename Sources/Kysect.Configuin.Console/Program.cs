3d4237849e4784527271a2d5c1f91b1c3ecc9f7b
Fredi Kats <fredikats@outlook.com>
2023-09-02 19:24:58 +0400

    Setup DI for running console application for generation codestyle

--- Sources/Kysect.Configuin.Console/Program.cs
+++ Sources/Kysect.Configuin.Console/Program.cs
@@ -1,28 +1,52 @@
 ﻿using Kysect.Configuin.Common;
 using Kysect.Configuin.Console.Configuration;
 using Kysect.Configuin.Core.CodeStyleGeneration;
+using Kysect.Configuin.Core.CodeStyleGeneration.Markdown;
 using Kysect.Configuin.Core.CodeStyleGeneration.Models;
 using Kysect.Configuin.Core.EditorConfigParsing;
+using Kysect.Configuin.Core.MarkdownParsing.TextExtractor;
 using Kysect.Configuin.Core.MsLearnDocumentation;
 using Kysect.Configuin.Core.MsLearnDocumentation.Models;
 using Kysect.Configuin.Core.RoslynRuleModels;
+using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Options;
 
-IServiceProvider serviceProvider = InitializeServiceProvider();
-GenerateCodeStyle(serviceProvider);
+GenerateCodeStyle(InitializeServiceProvider());
 
 IServiceProvider InitializeServiceProvider()
 {
-    var serviceCollection = new ServiceCollection();
+    HostApplicationBuilder builder = Host.CreateApplicationBuilder();
+    builder.Configuration.AddJsonFile("appsettings.json");
+
+    var serviceCollection = builder.Services;
 
     serviceCollection.AddOptionsWithValidation<ConfiguinConfiguration>(nameof(ConfiguinConfiguration));
+
+    serviceCollection.AddSingleton<IEditorConfigContentProvider>(sp =>
+    {
+        IOptions<ConfiguinConfiguration> options = sp.GetRequiredService<IOptions<ConfiguinConfiguration>>();
+
+        return new EditorConfigFileContentProvider(options.Value.EditorConfigFile);
+    });
+
+    serviceCollection.AddSingleton<IEditorConfigSettingsParser, EditorConfigSettingsParser>();
 
     serviceCollection.AddSingleton<IMsLearnDocumentationInfoProvider>(sp =>
     {
         IOptions<ConfiguinConfiguration> options = sp.GetRequiredService<IOptions<ConfiguinConfiguration>>();
 
         return new MsLearnDocumentationInfoLocalProvider(options.Value.MsLearnRepositoryPath);
+    });
+
+    serviceCollection.AddSingleton<IMsLearnDocumentationParser>(_ => new MsLearnDocumentationParser(PlainTextExtractor.Create()));
+    serviceCollection.AddSingleton<ICodeStyleGenerator, CodeStyleGenerator>();
+    serviceCollection.AddSingleton<ICodeStyleWriter>(sp =>
+    {
+        IOptions<ConfiguinConfiguration> options = sp.GetRequiredService<IOptions<ConfiguinConfiguration>>();
+
+        return new MarkdownCodeStyleWriter(options.Value.OutputPath);
     });
 
     return serviceCollection.BuildServiceProvider();
---
+++ Sources/Kysect.Configuin.Core/EditorConfigParsing/EditorConfigFileContentProvider.cs
@@ -0,0 +1,16 @@
+﻿namespace Kysect.Configuin.Core.EditorConfigParsing;
+
+public class EditorConfigFileContentProvider : IEditorConfigContentProvider
+{
+    private readonly string _filePath;
+
+    public EditorConfigFileContentProvider(string filePath)
+    {
+        _filePath = filePath;
+    }
+
+    public string Provide()
+    {
+        return File.ReadAllText(_filePath);
+    }
+}
