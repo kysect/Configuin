a127651fc52d6a2cb7734953a87777dd2152e5f2
Fredi Kats <fredikats@outlook.com>
2023-09-02 16:09:46 +0400

    Fix rule id parsing

--- Sources/Kysect.Configuin.Common/StringExtensions.cs
+++ Sources/Kysect.Configuin.Common/StringExtensions.cs
@@ -6,7 +6,7 @@ public static class StringExtensions
 {
     public static string WithoutPrefix(this string value, string prefix)
     {
-        if (!value.StartsWith(prefix))
+        if (!value.StartsWith(prefix, StringComparison.InvariantCultureIgnoreCase))
             throw new ArgumentException($"String {value} does not start with {prefix}");
 
         return value.Substring(prefix.Length, value.Length - prefix.Length);
