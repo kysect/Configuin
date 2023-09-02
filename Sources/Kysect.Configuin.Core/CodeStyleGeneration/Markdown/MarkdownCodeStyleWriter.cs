bdf060ec3b9b5023938194ba76c42024f861ad67
Fredi Kats <fredikats@outlook.com>
2023-09-02 19:24:04 +0400

    Implement markdown export

---
+++ Sources/Kysect.Configuin.Core/CodeStyleGeneration/Markdown/MarkdownCodeStyleWriter.cs
@@ -0,0 +1,21 @@
+﻿using Kysect.Configuin.Core.CodeStyleGeneration.Models;
+
+namespace Kysect.Configuin.Core.CodeStyleGeneration.Markdown;
+
+public class MarkdownCodeStyleWriter : ICodeStyleWriter
+{
+    private readonly MarkdownCodeStyleFormatter _formatter = new MarkdownCodeStyleFormatter();
+
+    private readonly string _filePath;
+
+    public MarkdownCodeStyleWriter(string filePath)
+    {
+        _filePath = filePath;
+    }
+
+    public void Write(CodeStyle codeStyle)
+    {
+        string content = _formatter.Format(codeStyle);
+        File.WriteAllText(_filePath, content);
+    }
+}
