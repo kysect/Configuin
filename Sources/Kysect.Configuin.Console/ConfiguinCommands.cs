a3c1ebc971b66b0884cd8414b8aba0b2b16e0102
Fredi Kats <fredikats@outlook.com>
2023-09-03 16:39:23 +0400

    Decompose Program.cs in console project

---
+++ Sources/Kysect.Configuin.Console/ConfiguinCommands.cs
@@ -0,0 +1,38 @@
+﻿using Kysect.Configuin.Core.CodeStyleGeneration;
+using Kysect.Configuin.Core.CodeStyleGeneration.Models;
+using Kysect.Configuin.Core.EditorConfigParsing;
+using Kysect.Configuin.Core.MsLearnDocumentation;
+using Kysect.Configuin.Core.MsLearnDocumentation.Models;
+using Kysect.Configuin.Core.RoslynRuleModels;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace Kysect.Configuin.Console;
+
+public class ConfiguinCommands
+{
+    private readonly IServiceProvider _serviceProvider;
+
+    public ConfiguinCommands(IServiceProvider serviceProvider)
+    {
+        _serviceProvider = serviceProvider;
+    }
+
+    public void GenerateCodeStyle()
+    {
+        IEditorConfigContentProvider editorConfigContentProvider = _serviceProvider.GetRequiredService<IEditorConfigContentProvider>();
+        IEditorConfigSettingsParser editorConfigSettingsParser = _serviceProvider.GetRequiredService<IEditorConfigSettingsParser>();
+        IMsLearnDocumentationInfoProvider msLearnDocumentationInfoProvider = _serviceProvider.GetRequiredService<IMsLearnDocumentationInfoProvider>();
+        IMsLearnDocumentationParser msLearnDocumentationParser = _serviceProvider.GetRequiredService<IMsLearnDocumentationParser>();
+        ICodeStyleGenerator codeStyleGenerator = _serviceProvider.GetRequiredService<ICodeStyleGenerator>();
+        ICodeStyleWriter codeStyleWriter = _serviceProvider.GetRequiredService<ICodeStyleWriter>();
+
+        string editorConfigContent = editorConfigContentProvider.Provide();
+        EditorConfigSettings editorConfigSettings = editorConfigSettingsParser.Parse(editorConfigContent);
+
+        MsLearnDocumentationRawInfo msLearnDocumentationRawInfo = msLearnDocumentationInfoProvider.Provide();
+        RoslynRules roslynRules = msLearnDocumentationParser.Parse(msLearnDocumentationRawInfo);
+
+        CodeStyle codeStyle = codeStyleGenerator.Generate(editorConfigSettings, roslynRules);
+        codeStyleWriter.Write(codeStyle);
+    }
+}
---
+++ Sources/Kysect.Configuin.Console/DependencyBuilder.cs
@@ -0,0 +1,53 @@
+﻿using Kysect.Configuin.Common;
+using Kysect.Configuin.Console.Configuration;
+using Kysect.Configuin.Core.CodeStyleGeneration;
+using Kysect.Configuin.Core.CodeStyleGeneration.Markdown;
+using Kysect.Configuin.Core.EditorConfigParsing;
+using Kysect.Configuin.Core.MarkdownParsing.TextExtractor;
+using Kysect.Configuin.Core.MsLearnDocumentation;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Options;
+
+namespace Kysect.Configuin.Console;
+
+public class DependencyBuilder
+{
+    public static IServiceProvider InitializeServiceProvider()
+    {
+        HostApplicationBuilder builder = Host.CreateApplicationBuilder();
+        builder.Configuration.AddJsonFile("appsettings.json");
+
+        IServiceCollection serviceCollection = builder.Services;
+
+        serviceCollection.AddOptionsWithValidation<ConfiguinConfiguration>(nameof(ConfiguinConfiguration));
+
+        serviceCollection.AddSingleton<IEditorConfigContentProvider>(sp =>
+        {
+            IOptions<ConfiguinConfiguration> options = sp.GetRequiredService<IOptions<ConfiguinConfiguration>>();
+
+            return new EditorConfigFileContentProvider(options.Value.EditorConfigFile);
+        });
+
+        serviceCollection.AddSingleton<IEditorConfigSettingsParser, EditorConfigSettingsParser>();
+
+        serviceCollection.AddSingleton<IMsLearnDocumentationInfoProvider>(sp =>
+        {
+            IOptions<ConfiguinConfiguration> options = sp.GetRequiredService<IOptions<ConfiguinConfiguration>>();
+
+            return new MsLearnDocumentationInfoLocalProvider(options.Value.MsLearnRepositoryPath);
+        });
+
+        serviceCollection.AddSingleton<IMsLearnDocumentationParser>(_ => new MsLearnDocumentationParser(PlainTextExtractor.Create()));
+        serviceCollection.AddSingleton<ICodeStyleGenerator, CodeStyleGenerator>();
+        serviceCollection.AddSingleton<ICodeStyleWriter>(sp =>
+        {
+            IOptions<ConfiguinConfiguration> options = sp.GetRequiredService<IOptions<ConfiguinConfiguration>>();
+
+            return new MarkdownCodeStyleWriter(options.Value.OutputPath);
+        });
+
+        return serviceCollection.BuildServiceProvider();
+    }
+}
