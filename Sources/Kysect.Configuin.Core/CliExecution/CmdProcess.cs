b9f79bf321de171ec3219880674837b3e68bbca2
Fredi Kats <fredikats@outlook.com>
2023-09-05 23:53:11 +0400

    Implement dotnet format execution in separated process

---
+++ Sources/Kysect.Configuin.Core/CliExecution/CmdProcess.cs
@@ -0,0 +1,56 @@
+﻿using Kysect.CommonLib.BaseTypes.Extensions;
+using System.Diagnostics;
+
+namespace Kysect.Configuin.Core.CliExecution;
+
+public class CmdProcess
+{
+    public async Task ExecuteCommand(string command)
+    {
+        var exceptions = new List<Exception>();
+
+        using var process = new Process();
+
+        process.Exited += (sender, _) =>
+        {
+            if (sender is null)
+            {
+                exceptions.Add(new NullReferenceException(nameof(sender)));
+                return;
+            }
+
+            int exitCode = sender.To<Process>().ExitCode;
+            if (exitCode != 0)
+            {
+                exceptions.Add(new CmdProcessException($"Application return exit code {exitCode}."));
+            }
+        };
+
+        process.ErrorDataReceived += (sender, args) =>
+        {
+            if (args.Data is not null)
+                exceptions.Add(new CmdProcessException());
+        };
+
+        var startInfo = new ProcessStartInfo
+        {
+            WindowStyle = ProcessWindowStyle.Hidden,
+            RedirectStandardError = true,
+            FileName = "cmd.exe",
+            Arguments = $"/C {command}"
+        };
+
+        process.StartInfo = startInfo;
+        process.Start();
+        await process.WaitForExitAsync();
+
+        string errors = await process.StandardError.ReadToEndAsync();
+        if (!string.IsNullOrEmpty(errors))
+            exceptions.Add(new CmdProcessException(errors));
+
+        process.Close();
+
+        if (exceptions.Count > 0)
+            throw new AggregateException("Failed to execute cmd command", exceptions);
+    }
+}
---
+++ Sources/Kysect.Configuin.Tests/DotnetFormat/DotnetFormatCliTests.cs
@@ -0,0 +1,29 @@
+﻿using Kysect.Configuin.Core.DotnetFormat;
+using NUnit.Framework;
+
+namespace Kysect.Configuin.Tests.DotnetFormat;
+
+public class DotnetFormatCliTests
+{
+    private readonly DotnetFormatCli _dotnetFormatCli;
+
+    public DotnetFormatCliTests()
+    {
+        _dotnetFormatCli = new DotnetFormatCli();
+    }
+
+    [Test]
+    public void Validate_FinishedWithoutErrors()
+    {
+        _dotnetFormatCli.Validate();
+    }
+
+    [Test]
+    [Ignore("This test require infrastructure")]
+    public void GenerateWarnings_CreateReportFile()
+    {
+        const string pathToSln = "./../../../../";
+
+        _dotnetFormatCli.GenerateWarnings(pathToSln, "sample.json");
+    }
+}
