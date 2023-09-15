bafe05d99865461f8fd13d14e36e9fbd0030341b
Fredi Kats <fredikats@outlook.com>
2023-09-15 13:13:16 +0400

    Add roslyn rule model builder

---
+++ Sources/Kysect.Configuin.RoslynModels/RoslynRulesBuilder.cs
@@ -0,0 +1,41 @@
+﻿namespace Kysect.Configuin.RoslynModels;
+
+public class RoslynRulesBuilder
+{
+    private readonly List<RoslynQualityRule> _qualityRules;
+    private readonly List<RoslynStyleRuleGroup> _styleRuleGroups;
+
+    public RoslynRulesBuilder()
+    {
+        _qualityRules = new List<RoslynQualityRule>();
+        _styleRuleGroups = new List<RoslynStyleRuleGroup>();
+    }
+
+    public static RoslynRulesBuilder New()
+    {
+        return new RoslynRulesBuilder();
+    }
+
+    public RoslynRulesBuilder AddQuality(RoslynQualityRule qualityRule)
+    {
+        _qualityRules.Add(qualityRule);
+        return this;
+    }
+
+    public RoslynRulesBuilder AddStyle(RoslynStyleRule styleRule)
+    {
+        _styleRuleGroups.Add(new RoslynStyleRuleGroup(new[] { styleRule }));
+        return this;
+    }
+
+    public RoslynRulesBuilder AddStyle(RoslynStyleRuleGroup styleRuleGroup)
+    {
+        _styleRuleGroups.Add(styleRuleGroup);
+        return this;
+    }
+
+    public RoslynRules Build()
+    {
+        return new RoslynRules(_qualityRules, _styleRuleGroups);
+    }
+}
