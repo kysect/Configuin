df3611f123a1acdd1e865b04b89d10ea75a700ef
Fredi Kats <fredikats@outlook.com>
2024-05-16 08:31:25 +0200

    Add EditorConfigDocument node rewriter

---
+++ Sources/Kysect.Configuin.EditorConfig/DocumentModel/EditorConfigDocumentNodeRewriter.cs
@@ -0,0 +1,73 @@
+﻿using Kysect.CommonLib.BaseTypes.Extensions;
+using Kysect.Configuin.EditorConfig.DocumentModel.Nodes;
+using System.Collections.Immutable;
+
+namespace Kysect.Configuin.EditorConfig.DocumentModel;
+
+public static class EditorConfigDocumentNodeRewriter
+{
+    public static EditorConfigDocument RemoveNodes(this EditorConfigDocument document, IReadOnlyCollection<IEditorConfigNode> nodes)
+    {
+        return (EditorConfigDocument) document.FilterChildren(nodes);
+    }
+
+    public static EditorConfigDocument ReplaceNodes(this EditorConfigDocument document, IEditorConfigNode oldNode, IEditorConfigNode newNode)
+    {
+        return (EditorConfigDocument) document.ReplaceChildren(oldNode, newNode);
+    }
+
+    public static EditorConfigDocument UpdateNodes(this EditorConfigDocument document, Func<IEditorConfigNode, IEditorConfigNode> morphimsm)
+    {
+        document.ThrowIfNull();
+        morphimsm.ThrowIfNull();
+
+        return (EditorConfigDocument) document.UpdateChildren(morphimsm);
+    }
+
+
+    private static IEditorConfigNode FilterChildren(this IEditorConfigNode node, IReadOnlyCollection<IEditorConfigNode> nodes)
+    {
+        if (node is not IEditorConfigContainerNode containerNode)
+            return node;
+
+        ImmutableList<IEditorConfigNode> newChildren = [];
+        foreach (IEditorConfigNode editorConfigNode in containerNode.Children)
+        {
+            if (nodes.Contains(editorConfigNode))
+                continue;
+
+            newChildren = newChildren.Add(editorConfigNode.FilterChildren(nodes));
+        }
+
+        return containerNode.WithChildren(newChildren);
+    }
+
+    private static IEditorConfigNode ReplaceChildren(this IEditorConfigNode currentNode, IEditorConfigNode oldNode, IEditorConfigNode newNode)
+    {
+        if (currentNode == oldNode)
+            return newNode;
+
+        if (currentNode is not IEditorConfigContainerNode containerNode)
+            return currentNode;
+
+        ImmutableList<IEditorConfigNode> newChildren = [];
+        foreach (IEditorConfigNode editorConfigNode in containerNode.Children)
+            newChildren = newChildren.Add(editorConfigNode.ReplaceChildren(oldNode, newNode));
+
+        return containerNode.WithChildren(newChildren);
+    }
+
+    private static IEditorConfigNode UpdateChildren(this IEditorConfigNode currentNode, Func<IEditorConfigNode, IEditorConfigNode> morphimsm)
+    {
+        currentNode = morphimsm(currentNode);
+
+        if (currentNode is not IEditorConfigContainerNode containerNode)
+            return currentNode;
+
+        ImmutableList<IEditorConfigNode> newChildren = containerNode
+            .Children
+            .Select(morphimsm)
+            .ToImmutableList();
+        return containerNode.WithChildren(newChildren);
+    }
+}
---
+++ Sources/Kysect.Configuin.Tests/EditorConfig/EditorConfigDocumentNodeRewriterTests.cs
@@ -0,0 +1,82 @@
+﻿using Kysect.CommonLib.BaseTypes.Extensions;
+using Kysect.Configuin.EditorConfig.DocumentModel;
+using Kysect.Configuin.EditorConfig.DocumentModel.Nodes;
+using Kysect.Configuin.Tests.EditorConfig.Tools;
+
+namespace Kysect.Configuin.Tests.EditorConfig;
+
+public class EditorConfigDocumentNodeRewriterTests
+{
+    private readonly EditorConfigDocumentComparator _comparator = new EditorConfigDocumentComparator();
+
+    [Fact]
+    public void Remove_Child_ReturnDocumentWithoutChild()
+    {
+        EditorConfigDocument input = new EditorConfigDocument()
+            .AddChild(new EditorConfigPropertyNode("first", "value"))
+            .AddChild(new EditorConfigPropertyNode("second", "value"));
+
+        EditorConfigDocument expected = new EditorConfigDocument()
+            .AddChild(new EditorConfigPropertyNode("second", "value"));
+
+        var nodeForRemoving = input.Children.First();
+        EditorConfigDocument actual = input.RemoveNodes([nodeForRemoving]);
+
+        _comparator.Compare(actual, expected);
+    }
+
+    [Fact]
+    public void Remove_MultipleChild_ReturnDocumentWithoutChild()
+    {
+        EditorConfigDocument input = new EditorConfigDocument()
+            .AddChild(new EditorConfigPropertyNode("first", "value"))
+            .AddChild(new EditorConfigPropertyNode("second", "value"))
+            .AddChild(new EditorConfigPropertyNode("third", "value"));
+
+        EditorConfigDocument expected = new EditorConfigDocument()
+            .AddChild(new EditorConfigPropertyNode("third", "value"));
+
+        var nodesForRemoving = input.Children.Take(2).ToList();
+        EditorConfigDocument actual = input.RemoveNodes(nodesForRemoving);
+
+        _comparator.Compare(actual, expected);
+    }
+
+    [Fact]
+    public void ReplaceNode_NodeWithChild_ReplaceAll()
+    {
+        EditorConfigDocument input = new EditorConfigDocument()
+            .AddChild(new EditorConfigDocumentSectionNode("### Section ###")
+                .AddChild(new EditorConfigPropertyNode("first", "value")));
+
+        EditorConfigDocument expected = new EditorConfigDocument()
+            .AddChild(new EditorConfigDocumentSectionNode("### Section ###")
+                .AddChild(new EditorConfigPropertyNode("second", "value2")));
+
+        var nodesForRemoving = input.Children.First().To<IEditorConfigContainerNode>().Children.First();
+        EditorConfigDocument actual = input.ReplaceNodes(nodesForRemoving, new EditorConfigPropertyNode("second", "value2"));
+
+        _comparator.Compare(actual, expected);
+    }
+
+    [Fact]
+    public void UpdateNodes_NodeWithChild_OnlyParentChanged()
+    {
+        EditorConfigDocument input = new EditorConfigDocument()
+            .AddChild(new EditorConfigDocumentSectionNode("### Section ###")
+                .AddChild(new EditorConfigPropertyNode("first", "value")));
+
+        EditorConfigDocument expected = new EditorConfigDocument()
+            .AddChild(new EditorConfigDocumentSectionNode("### Other section ###")
+                .AddChild(new EditorConfigPropertyNode("first", "value")));
+
+        EditorConfigDocument actual = input.UpdateNodes(n =>
+        {
+            if (n is EditorConfigDocumentSectionNode { Value: "### Section ###" } oldSection)
+                return oldSection with { Value = "### Other section ###" };
+            return n;
+        });
+
+        _comparator.Compare(actual, expected);
+    }
+}
