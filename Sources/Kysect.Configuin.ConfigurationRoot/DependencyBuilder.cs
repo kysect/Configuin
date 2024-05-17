e8b8d36f2ee50fa42957697157966f08b3b52892
Fredi Kats <fredikats@outlook.com>
2024-05-17 10:27:03 +0200

    Add CLI command for formatting editorconfig file

--- Sources/Kysect.Configuin.ConfigurationRoot/DependencyBuilder.cs
+++ Sources/Kysect.Configuin.ConfigurationRoot/DependencyBuilder.cs
@@ -1,11 +1,11 @@
-﻿using Kysect.CommonLib.DependencyInjection;
-using Kysect.Configuin.CodeStyleDoc;
+﻿using Kysect.Configuin.CodeStyleDoc;
 using Kysect.Configuin.CodeStyleDoc.Markdown;
-using Kysect.Configuin.ConfigurationRoot.Configuration;
 using Kysect.Configuin.DotnetFormatIntegration;
 using Kysect.Configuin.DotnetFormatIntegration.Cli;
 using Kysect.Configuin.DotnetFormatIntegration.FileSystem;
 using Kysect.Configuin.EditorConfig;
+using Kysect.Configuin.EditorConfig.DocumentModel;
+using Kysect.Configuin.EditorConfig.Formatter;
 using Kysect.Configuin.EditorConfig.Template;
 using Kysect.Configuin.Markdown.TextExtractor;
 using Kysect.Configuin.MsLearn;
@@ -28,8 +28,6 @@ public static class DependencyBuilder
 
         IServiceCollection serviceCollection = builder.Services;
 
-        serviceCollection.AddOptionsWithValidation<ConfiguinConfiguration>(nameof(ConfiguinConfiguration));
-        serviceCollection.AddOptionsWithValidation<EditorConfigApplyConfiguration>(nameof(EditorConfigApplyConfiguration));
         serviceCollection.AddSingleton(CreateLogger);
 
         serviceCollection.AddSingleton<IDotnetConfigSettingsParser, DotnetConfigSettingsParser>();
@@ -48,6 +46,8 @@ public static class DependencyBuilder
         serviceCollection.AddSingleton<DotnetFormatReportComparator>();
         serviceCollection.AddSingleton<DotnetFormatPreviewGenerator>();
         serviceCollection.AddSingleton<EditorConfigTemplateGenerator>();
+        serviceCollection.AddSingleton<EditorConfigDocumentParser>();
+        serviceCollection.AddSingleton<EditorConfigFormatter>();
 
         return serviceCollection;
     }
---
+++ Sources/Kysect.Configuin.Console/Commands/FormatEditorconfigCommand.cs
@@ -0,0 +1,46 @@
+﻿using Kysect.CommonLib.BaseTypes.Extensions;
+using Kysect.Configuin.EditorConfig.DocumentModel;
+using Kysect.Configuin.EditorConfig.DocumentModel.Nodes;
+using Kysect.Configuin.EditorConfig.Formatter;
+using Kysect.Configuin.MsLearn;
+using Kysect.Configuin.MsLearn.Models;
+using Kysect.Configuin.RoslynModels;
+using Spectre.Console.Cli;
+using System.ComponentModel;
+
+namespace Kysect.Configuin.Console.Commands;
+
+internal sealed class FormatEditorconfigCommand(
+    IMsLearnDocumentationInfoReader repositoryPathReader,
+    IMsLearnDocumentationParser msLearnDocumentationParser,
+    EditorConfigDocumentParser editorConfigDocumentParser,
+    EditorConfigFormatter editorConfigFormatter) : Command<FormatEditorconfigCommand.Settings>
+{
+    public sealed class Settings : CommandSettings
+    {
+        [Description("Path to editorconfig.")]
+        [CommandArgument(0, "[editor config path]")]
+        public string? EditorConfigPath { get; init; }
+
+        [Description("Path to cloned MS Learn repository.")]
+        [CommandOption("-d|--documentation")]
+        public string? MsLearnRepositoryPath { get; init; }
+    }
+
+    public override int Execute(CommandContext context, Settings settings)
+    {
+        settings.EditorConfigPath.ThrowIfNull();
+        settings.MsLearnRepositoryPath.ThrowIfNull();
+
+        string editorConfigContent = File.ReadAllText(settings.EditorConfigPath);
+        string[] editorConfigContentLines = File.ReadAllLines(settings.EditorConfigPath);
+
+        MsLearnDocumentationRawInfo msLearnDocumentationRawInfo = repositoryPathReader.Provide(settings.MsLearnRepositoryPath);
+        RoslynRules roslynRules = msLearnDocumentationParser.Parse(msLearnDocumentationRawInfo);
+        EditorConfigDocument editorConfigDocument = editorConfigDocumentParser.Parse(editorConfigContentLines);
+        EditorConfigDocument formattedDocument = editorConfigFormatter.FormatAccordingToRuleDefinitions(editorConfigDocument, roslynRules);
+        File.WriteAllText(settings.EditorConfigPath, formattedDocument.ToFullString());
+
+        return 0;
+    }
+}
