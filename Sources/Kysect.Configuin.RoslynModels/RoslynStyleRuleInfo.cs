856a008f430aad8718029ea83a855d67cb1dff2e
Fredi Kats <fredikats@outlook.com>
2024-05-17 23:29:17 +0200

    Add support for naming rule

---
+++ Sources/Kysect.Configuin.RoslynModels/RoslynStyleRuleInfo.cs
@@ -0,0 +1,18 @@
+﻿using Kysect.CommonLib.BaseTypes.Extensions;
+
+namespace Kysect.Configuin.RoslynModels;
+
+public static class RoslynNameRuleInfo
+{
+    public static RoslynRuleId RuleId { get; } = new RoslynRuleId("IDE", 1006);
+
+    public static bool IsNameRuleOption(string optionName)
+    {
+        optionName.ThrowIfNull();
+
+        return optionName.StartsWith("dotnet_naming_rule")
+               || optionName.StartsWith("dotnet_naming_symbols")
+               || optionName.StartsWith("dotnet_naming_style");
+
+    }
+}
